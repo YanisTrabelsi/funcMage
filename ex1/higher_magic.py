98161f90a402ce86022739472e6e829d4254d1ea
ytrabels <ytrabels@f1br2s2.paris.42.school>
2026-08-03 16:26:33 +0200

    mypy done

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,27 +1,34 @@
 from collections.abc import Callable
 
-def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
-    def combine(target: str, power: int):
+
+def spell_combiner(spell1: Callable[[str, int], str],
+                   spell2: Callable[[str, int], str])\
+                   -> Callable[[str, int], tuple[str, str]]:
+    def combine(target: str, power: int) -> tuple[str, str]:
         return ((spell1(target, power), spell2(target, power)))
     return (combine)
 
 
-def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    def amplifie(target: str, power: int):
+def power_amplifier(base_spell: Callable[[str, int], str],
+                    multiplier: int) -> Callable[[str, int], str]:
+    def amplifie(target: str, power: int) -> str:
         return base_spell(target, power * multiplier)
     return amplifie
 
 
-def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    def conditional_cast(target: str, power: int):
+def conditional_caster(condition: Callable[[str, int], bool],
+                       spell: Callable[[str, int], str])\
+                       -> Callable[[str, int], str]:
+    def conditional_cast(target: str, power: int) -> str:
         if (condition(target, power)):
             return spell(target, power)
         return "Spell fizzled"
     return conditional_cast
 
 
-def spell_sequence(spells: list[Callable]) -> Callable:
-    def sequence(target: str, power: int):
+def spell_sequence(spells: list[Callable[[str, int], str]])\
+                   -> Callable[[str, int], list[str]]:
+    def sequence(target: str, power: int) -> list[str]:
         result: list[str] = []
         for spell in spells:
             result.append(spell(target, power))
@@ -33,15 +40,16 @@ def heal(target: str, power: int) -> str:
     return f"Heal restores {target} for {power} HP"
 
 
-def fireball(target: str, power:int) -> str:
+def fireball(target: str, power: int) -> str:
     return f"Fireball deals {power} damages to {target}"
 
 
 if (__name__ == "__main__"):
-    combined: Callable = spell_combiner(heal, fireball)
-    amplified: Callable = power_amplifier(fireball, 3)
-    conditioned: Callable = conditional_caster(lambda target, power: power >= 60, heal)
-    sequenced: Callable = spell_sequence([heal, fireball, fireball, heal])
+    combined: Callable[[str, int], tuple[str, str]] = spell_combiner(heal, fireball)
+    amplified: Callable[[str, int], str] = power_amplifier(fireball, 3)
+    conditioned: Callable[[str, int], str] =\
+        conditional_caster(lambda target, power: power >= 60, heal)
+    sequenced: Callable[[str, int], list[str]] = spell_sequence([heal, fireball, fireball, heal])
     print("== Spell combiner==")
     print(combined("Dragon", 60))
     print("\n== Power amplifier ==")
